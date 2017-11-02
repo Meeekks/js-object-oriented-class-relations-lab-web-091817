3a5685570a86a4e9eb59303aee0b8176d017e9ed
github username <github email address>
2017-11-02 03:21:39 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,75 @@
+let store = {drivers: [], passengers: [], trips: []};
+
+let driverId = 0;
+
+class Driver {
+  constructor(name) {
+    this.id = ++driverId;
+    this.name = name;
+    store.drivers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter(function(trip) {
+      return trip.driverId === this.id;
+    }.bind(this));
+  }
+
+  passengers() {
+    return store.trips.map(function(trip) {
+      if (trip.driverId === this.id) {
+        return trip.passenger();
+      }
+     }.bind(this));
+   }
+
+}
+
+let passengerId = 0;
+
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerId;
+    this.name = name;
+    store.passengers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter(function(trip) {
+      return trip.passengerId === this.id;
+    }.bind(this));
+  }
+
+  drivers() {
+    return store.trips.map(function(trip) {
+      if (trip.passengerId === this.id) {
+        return trip.driver();
+      }
+     }.bind(this));
+  }
+
+}
+
+let tripId = 0;
+
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId;
+    if (driver) {this.driverId = driver.id;}
+    if (passenger) {this.passengerId = passenger.id;}
+    store.trips.push(this);
+  }
+
+  driver() {
+    return store.drivers.find(function(driver) {
+      return driver.id === this.driverId;
+    }.bind(this));
+  }
+
+  passenger() {
+    return store.passengers.find(function(passenger) {
+      return passenger.id === this.passengerId;
+    }.bind(this));
+  }
+
+}
